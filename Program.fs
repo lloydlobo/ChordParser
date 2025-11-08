11a85c76c3a11359732effbfae8456e57caced1b
Lloyd Lobo <lloydlobo4@gmail.com>
2025-11-08 15:36:57 +0530

    feat(Program): add dev tools

--- Program.fs
+++ Program.fs
@@ -1,12 +1,11 @@
-﻿open Avalonia
+namespace ChordParser
+
+open Avalonia // open Avalonia.Diagnostics
+open Avalonia.Input
 open Avalonia.Themes.Fluent
 open Avalonia.FuncUI.Hosts
 open Avalonia.Controls.ApplicationLifetimes
 open Avalonia.Controls
-// open Avalonia.FuncUI.Diagnostics
-
-open ChordParser.ChordParser
-open ChordParser.ChordParserView // provides: view()
 
 type MainWindow() =
     inherit HostWindow()
@@ -16,7 +15,7 @@ type MainWindow() =
         base.Icon <- WindowIcon (System.IO.Path.Combine ("Assets", "Icons", "icon.ico"))
         base.Width <- 800.0
         base.Height <- 450.0
-        base.Content <- view ()
+        base.Content <- ChordParserView.view ()
 
 type App() =
     inherit Application()
@@ -29,8 +28,10 @@ type App() =
         match this.ApplicationLifetime with
         | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
             let mainWindow = MainWindow ()
-            //InspectorWindow(mainWindow).Show()
-            //mainWindow.Renderer.DrawDirtyRects <- true
+#if DEBUG
+            mainWindow.AttachDevTools () // open dev tools with hotkey: Ctrl+Shift+D (use this instead of InspectorWindow(mainWindow).Show ())
+            // mainWindow.Renderer.DrawDirtyRects <- true
+#endif
             desktopLifetime.MainWindow <- mainWindow
         | _ -> ()
 
@@ -42,4 +43,5 @@ module Program =
             .Configure<App>()
             .UsePlatformDetect()
             .UseSkia()
-            .StartWithClassicDesktopLifetime (args)
+            .StartWithClassicDesktopLifetime
+            args
